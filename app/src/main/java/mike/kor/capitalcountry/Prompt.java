0e9ee162429d44eeb9c3e1dfd00ebb9819f3eadd
Mike-Tigrov <xiwnik@yahoo.com>
2022-07-19 07:06:26 +0300

    Add link to feedback. Some changes in code (colors to the great contrast).

--- app/src/main/java/mike/kor/capitalcountry/Prompt.java
+++ app/src/main/java/mike/kor/capitalcountry/Prompt.java
@@ -2,7 +2,10 @@ package mike.kor.capitalcountry;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +15,11 @@ public class Prompt extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.prompt);
+
+        //Добавляем ссылку на телеграм
+        TextView telegram = (TextView) findViewById(R.id.telegramText2);
+        telegram.setText("https://t.me/CountryCapital_app");
+        Linkify.addLinks(telegram, Linkify.ALL);
     }
     public void goToBaseKnowledge(View v){
         Intent intent = new Intent(this, BaseKnowledge.class);
@@ -22,4 +30,6 @@ public class Prompt extends AppCompatActivity {
         Intent intent = new Intent(this, ListTren.class);
         startActivity(intent);
     }
+
+
 }
