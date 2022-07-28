8205869cfd1e4094111e24dc2e03d5b1c917eedf
Mike-Tigrov <xiwnik@yahoo.com>
2022-07-28 11:17:58 +0300

    New version app. Start add Privacy Policy.

---
+++ app/src/main/java/mike/kor/capitalcountry/PrivacyPolicy.java
@@ -0,0 +1,14 @@
+package mike.kor.capitalcountry;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PrivacyPolicy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_privacy_policy);
+    }
+}
