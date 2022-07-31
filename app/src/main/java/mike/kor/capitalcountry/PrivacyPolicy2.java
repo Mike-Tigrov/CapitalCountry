17ed583a2a9a100ddb0816ed916e9edab2eb3c93
Mike-Tigrov <xiwnik@yahoo.com>
2022-07-31 22:30:35 +0300

    Add text Privacy Policy to new activity.

--- app/src/main/java/mike/kor/capitalcountry/PrivacyPolicy2.java
+++ app/src/main/java/mike/kor/capitalcountry/PrivacyPolicy2.java
@@ -1,7 +1,10 @@
 package mike.kor.capitalcountry;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class PrivacyPolicy2 extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class PrivacyPolicy2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_privacy_policy2);
+    }
+    public void goToBaseKnowledge(View v){
+        Intent intent = new Intent(this, BaseKnowledge.class);
+        startActivity(intent);
+    }
+    public void goToListTren (View v){
+        Intent intent = new Intent(this, ListTren.class);
+        startActivity(intent);
+    }
+    public void goToPrivacyPolicy1 (View v){
+        Intent intent = new Intent(this, PrivacyPolicy1.class);
+        startActivity(intent);
+    }
+    public void goToPrivacyPolicy (View v){
+        Intent intent = new Intent(this, PrivacyPolicy.class);
+        startActivity(intent);
     }
 }
