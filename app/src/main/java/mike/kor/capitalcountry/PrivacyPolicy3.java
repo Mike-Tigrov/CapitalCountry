bc8d7fde92721928542b55331462dd5fae840fe8
Mike-Tigrov <xiwnik@yahoo.com>
2022-08-05 10:10:04 +0300

    Add text Privacy Policy to new activity.

--- app/src/main/java/mike/kor/capitalcountry/PrivacyPolicy3.java
+++ app/src/main/java/mike/kor/capitalcountry/PrivacyPolicy3.java
@@ -1,6 +1,8 @@
 package mike.kor.capitalcountry;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,21 @@ public class PrivacyPolicy3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_privacy_policy3);
+    }
+    public void goToBaseKnowledge(View v){
+        Intent intent = new Intent(this, BaseKnowledge.class);
+        startActivity(intent);
+    }
+    public void goToListTren (View v){
+        Intent intent = new Intent(this, ListTren.class);
+        startActivity(intent);
+    }
+    public void goToPrivacyPolicy2 (View v){
+        Intent intent = new Intent(this, PrivacyPolicy1.class);
+        startActivity(intent);
+    }
+    public void goToPrivacyPolicy1 (View v){
+        Intent intent = new Intent(this, PrivacyPolicy.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/mike/kor/capitalcountry/PrivacyPolicy4.java
+++ app/src/main/java/mike/kor/capitalcountry/PrivacyPolicy4.java
@@ -1,6 +1,8 @@
 package mike.kor.capitalcountry;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,4 +13,17 @@ public class PrivacyPolicy4 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_privacy_policy4);
     }
+    public void goToBaseKnowledge(View v){
+        Intent intent = new Intent(this, BaseKnowledge.class);
+        startActivity(intent);
+    }
+    public void goToListTren (View v){
+        Intent intent = new Intent(this, ListTren.class);
+        startActivity(intent);
+    }
+    public void goToPrivacyPolicy3 (View v){
+        Intent intent = new Intent(this, PrivacyPolicy3.class);
+        startActivity(intent);
+    }
+
 }
